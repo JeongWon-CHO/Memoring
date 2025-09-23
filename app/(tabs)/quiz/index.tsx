13d9544e30d6d51869b79aca20f56c36771b3673
JeongWon-CHO <ahin0312@naver.com>
2025-09-23 15:38:13 +0900

    feat: 임시 퀴즈 진입점 추가

--- app/(tabs)/quiz/index.tsx
+++ app/(tabs)/quiz/index.tsx
@@ -1,6 +1,9 @@
+import Header from '@/components/common/Header';
+import { colors } from '@/constants/colors';
+import { typography } from '@/constants/typography';
+import Feather from '@expo/vector-icons/Feather';
 import { useFocusEffect } from 'expo-router';
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function MyScreen() {
   useFocusEffect(() => {
@@ -8,8 +11,69 @@ export default function MyScreen() {
   })
 
   return (
-    <SafeAreaView>
-      <Text>퀴즈 스크린</Text>
-    </SafeAreaView>
+    <View>
+      <Header title='퀴즈' />
+      
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={typography.B1_BOLD}>퀴즈를 풀어볼까요?</Text>
+        </View>
+
+        <View style={styles.icon}>
+          <Feather name="check-circle" size={64} color="black" />
+        </View>
+        
+        <View style={styles.contentContainer}>
+          <View style={styles.quizInfo}>
+            <Text style={[typography.C1, styles.subText]}>총 10문제</Text>
+          </View>
+
+          <View style={styles.startButton}>
+            <Text style={styles.startButtonText}>시작하기</Text>
+          </View>
+        </View>
+      </View>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+  },
+  header: {
+    marginHorizontal: 20,
+    marginTop: 60,
+  },
+  contentContainer: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 100,
+    gap: 20,
+  },
+  icon: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 150,
+  },
+  quizInfo: {
+    marginVertical: 12,
+  },
+  subText: {
+    color: colors.GRAY_400,
+  },
+  startButton: {
+    width: 150,
+    paddingHorizontal: 8,
+    paddingVertical: 12,
+    backgroundColor: colors.MAIN,
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  startButtonText: {
+    color: colors.WHITE,
+  }
+});
